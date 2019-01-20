b5d10eb4afae9ecd28db0fd528c1ca32a1bfd318
Vitao18 <victoronline9@gmail.com>
2019-01-20 20:20:50 -0200

    fix: graphql not working with mjs files

--- users/main.mjs
+++ users/main.mjs
@@ -1,9 +1,10 @@
 import express from "express";
 import expressGraphQL from "express-graphql";
+import schema from "./schema/schema.js";
 
 const app = express();
 
-app.use("/graphql", expressGraphQL({ graphiql: true }));
+app.use("/graphql", expressGraphQL({ schema, graphiql: true }));
 
 app.get("/", (req, res) => res.json({ ok: true }));
 
--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,4 +1,14 @@
-import { GraphQLObjectType, GraphQLString, GraphQLInt } from "graphql";
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema
+} = require("graphql");
+
+const users = [
+  { id: "23", firstName: "Bill", age: 20 },
+  { id: "25", firstName: "Samantha", age: 23 }
+];
 
 const UserType = new GraphQLObjectType({
   name: "User",
@@ -8,3 +18,18 @@ const UserType = new GraphQLObjectType({
     age: { type: GraphQLInt }
   }
 });
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryTypes",
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return users.find(user => user.id === args.id);
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({ query: RootQuery });
