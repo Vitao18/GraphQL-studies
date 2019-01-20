aad11c3d2039a4d4a1c547857fec237afd4105c9
Vitao18 <victoronline9@gmail.com>
2019-01-20 19:20:49 -0200

    feat: defined User schema

--- users/main.mjs
+++ users/main.mjs
@@ -1,4 +1,10 @@
 import express from "express";
+import expressGraphQL from "express-graphql";
+
 const app = express();
+
+app.use("/graphql", expressGraphQL({ graphiql: true }));
+
 app.get("/", (req, res) => res.json({ ok: true }));
+
 app.listen(8080, () => console.log("ESM em com @std/esm"));
